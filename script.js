56dfc8b30360dec2a8bc355fc4f9a6f2192d1763
IvanPohorily777coder <thepatrickmaps@gmail.com>
2022-09-16 02:17:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+let numberOfFlims = prompt("Сколько фильмов Вы уже посмотрели?", '34');
+let filmName = prompt("Один из последних просмотренных фильмов", );
+let filmGrade = prompt("Какую оценку дадите?", '9.5');
+let filmName1 = prompt("Один из последних просмотренных фильмов", );
+let filmGrade1 = prompt("Какую оценку дадите?", '9.5');
+
+
+const personalMovieDB = {
+    count: numberOfFlims,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+
+personalMovieDB.movies[filmName] = filmGrade;
+personalMovieDB.movies[filmName2] = filmGrade2;
+
+ console.log(personalMovieDB);
+
+
+
